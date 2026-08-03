31f19fc0a8774dc9921e631d9ff9e1557168ad4b
imefo <imefo@yandex.ru>
2026-08-04 00:28:49 +0500

    feat: new cmd: init

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import subprocess
+from pathlib import Path
+
+home = Path.home()
+
+# XDG folders
+folder_config = home / ".config" / "idead"
+folder_data = home / ".local" / "share" / "idead"
+folder_cache = home / ".cache" / "idead"
+
+# Other folders
+folder_ideas = folder_data / "ideas"
+folder_posts = folder_data / "posts"
+folder_tasks = folder_data / "tasks"
+folder_guides = folder_data / "guides"
+
+def init():
+    folder_config.mkdir(parents=True, exist_ok=True)
+    folder_data.mkdir(parents=True, exist_ok=True)
+    folder_cache.mkdir(parents=True, exist_ok=True)
+
+    folder_ideas.mkdir(parents=True, exist_ok=True)
+    folder_posts.mkdir(parents=True, exist_ok=True)
+    folder_tasks.mkdir(parents=True, exist_ok=True)
+    folder_guides.mkdir(parents=True, exist_ok=True)
+    
