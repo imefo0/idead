1d56d2560b3aff5cb23aa8dee9807d5888ad34dc
imefo <imefo@yandex.ru>
2026-08-04 00:34:25 +0500

    feat: add init command handler

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import subprocess
 from pathlib import Path
+import sys
 
 home = Path.home()
 
@@ -15,6 +16,8 @@ folder_posts = folder_data / "posts"
 folder_tasks = folder_data / "tasks"
 folder_guides = folder_data / "guides"
 
+command = sys.argv[1:]
+
 def init():
     folder_config.mkdir(parents=True, exist_ok=True)
     folder_data.mkdir(parents=True, exist_ok=True)
@@ -24,4 +27,10 @@ def init():
     folder_posts.mkdir(parents=True, exist_ok=True)
     folder_tasks.mkdir(parents=True, exist_ok=True)
     folder_guides.mkdir(parents=True, exist_ok=True)
-    
+
+def main():
+    if command[0] == "init":
+        init()
+
+if __name__ == "__main__":
+    main()
