8f3bd8666451a1973a97b580d17af4d7d60b4b81
imefo <imefo@yandex.ru>
2026-08-08 11:58:51 +0500

    refactor: dry

--- main.py
+++ main.py
@@ -22,6 +22,10 @@ command = sys.argv[1:]
 
 ideas_format = "md"
 
+warning_choice = True
+
+delete_idea_ask = "Are you sure you want to delete this idea?"
+
 def init():
     folder_config.mkdir(parents=True, exist_ok=True)
     folder_data.mkdir(parents=True, exist_ok=True)
@@ -50,91 +54,64 @@ def get_list_of_files(folder):
 
     return files
 
-# TODO: добаввить функцию предупреждения чтобы можно было ее отключить через переменную auto_yes или choice_warning
-# TODO: добавить город в метаданных идей
+def warning(msg): # y, yes?, yn -> yes? [yn] y -> True
+    if warning_choice:
+        answer = input(f"{msg} [yn] ")
+        if answer.lower() in ["y", "yes"]:
+            return True
+        else:
+            return False
+    return True
 
-def remove_idea_by_date(date, time=False):
+# TODO: добаввить функцию предупреждения чтобы можно было ее отключить через переменную auto_yes или choice_warning
+# TODO: добавить город в метаданные идей
+
+def remove_idea_by_date(date=False, time=False):
     # WARN: код повторяется
-    if not time: # только по дате
-        coincidences = []
-        for i in get_list_of_files(folder_ideas): # i202608041359_dd0cd3.md -> i 2026-08-04 13:59 _ dd0cd3 .md
+
+    # if not time: # только по дате
+    coincidences = []
+    for i in get_list_of_files(folder_ideas): # i202608041359_dd0cd3.md -> i 2026-08-04 13:59 _ dd0cd3 .md
+        if not time:
             if i[1:9] == date:
                 coincidences.append(i)
+        else:
+            if not date:
+                if i[9:13] == time:
+                    coincidences.append(i)
+            else:
+                if i[1:9] == date and i[9:13] == time:
+                    coincidences.append(i)
 
-        if len(coincidences) == 0: # нет идей
-            # TODO: добавить класс Error и добавить эту ошибку как Error.NoMatchesFound
-            # TODO: добавить класс Error и добавить ошибку Error.IncorrectDate / IncorrectInput
-            print("E: No Matches Found")
+    if len(coincidences) == 0: # нет идей
+        # TODO: добавить класс Error и добавить эту ошибку как Error.NoMatchesFound
+        # TODO: добавить класс Error и добавить ошибку Error.IncorrectDate / IncorrectInput
+        print("E: No Matches Found")
 
-        elif len(coincidences) == 1:
-            # TODO: добавить так чтобы была понятна какая идея удаляется
-            answer = input("Are you sure you want to delete this idea? [yn] ")
+    elif len(coincidences) == 1:
+        # TODO: добавить так чтобы была понятна какая идея удаляется
 
-            if answer.lower() in ["y", "yes"]: # TODO: вынести список из y и yes в отдельную переменную для конфига
+        if warning(delete_idea_ask):
+            print("Deleting idea...")
+            (folder_ideas / coincidences[0]).unlink()
+    else: # много идей
+        # TODO: добавить удаление нескольких идей сразу
+
+        print(f"Found {len(coincidences)} ideas for{f" {date[:4]}-{date[4:6]}-{date[6:8]}" if date else ""}{f" {time[:2]}:{time[2:]}" if time else ""}:")
+        print("-"*30) # TODO: добавить переменную для этого числа (30)
+        for i in range(len(coincidences)): # i202608041359_dd0cd3 -> i 20260804 1359 _dd0cd3
+            date_coincidences = f"{coincidences[i][1:5]}-{coincidences[i][5:7]}-{coincidences[i][7:9]}"
+            time_coincidences = f"{coincidences[i][9:11]}:{coincidences[i][11:13]}"
+            print(f"  {i+1}. {date_coincidences} {time_coincidences} [{coincidences[i][14:-3]}]")
+
+        print("-"*30)
+        answer = int(input(f"Enter number to delete (1-{len(coincidences)}, or 0 to cancel): "))
+        # WARN: добавить проверку на число
+        if answer != 0:
+            new_answer = input("Are you sure you want to delete this idea? [yn] ")
+            if new_answer.lower() in ["y", "yes"]: # TODO: вынести список из y и yes в отдельную переменную для конфига
                 print("Deleting idea...")
-                (folder_ideas / coincidences[0]).unlink()
-
-        else: # много идей
-            # TODO: добавить удаление нескольких идей сразу
-            print(f"Found {len(coincidences)} ideas for {date[0:4]}-{date[4:6]}-{date[6:]}:")
-            print("-"*30) # TODO: добавить переменную для этого числа (30)
-
-            for i in range(len(coincidences)):
-                print(f"  {i+1}. {date[0:4]}-{date[4:6]}-{date[6:]} {coincidences[i][9:11]}:{coincidences[i][11:13]} [{coincidences[i][14:-3]}]")
-
-            print("-"*30)
-            # TEST: ПРОТЕСТИРОВАТЬ ЭТУ ДИЧЬ
-            answer = int(input(f"Enter number to delete (1-{len(coincidences)}, or 0 to cancel): "))
-            # WARN: добавить проверку на число
-            if answer != 0:
-                new_answer = input("Are you sure you want to delete this idea? [yn] ")
-                if new_answer.lower() in ["y", "yes"]: # TODO: вынести список из y и yes в отдельную переменную для конфига
-                    print("Deleting idea...")
-                    (folder_ideas / coincidences[answer-1]).unlink()
-
-
-    else: # по дате и времени
-        # 20260805 1356
-        if not date:
-            print("E: Date Not Entered")
-            return
-
-        coincidences = []
-        for i in get_list_of_files(folder_ideas): # i202608041359_dd0cd3.md -> i 2026-08-04 13:59 _ dd0cd3 .md
-            if i[1:9] == date and i[9:13] == time:
-                coincidences.append(i)
-
-        if len(coincidences) == 0: # нет идей
-            # TODO: добавить класс Error и добавить эту ошибку как Error.NoMatchesFound
-            # TODO: добавить класс Error и добавить ошибку Error.IncorrectDate / IncorrectInput
-            print("E: No Matches Found")
-
-        elif len(coincidences) == 1:
-            # TODO: добавить так чтобы была понятна какая идея удаляется
-            answer = input("Are you sure you want to delete this idea? [yn] ")
-
-            if answer.lower() in ["y", "yes"]: # TODO: вынести список из y и yes в отдельную переменную для конфига
-                print("Deleting idea...")
-                (folder_ideas / coincidences[0]).unlink()
-
-        else: # много идей
-            # TODO: добавить удаление нескольких идей сразу
-            print(f"Found {len(coincidences)} ideas for {date[0:4]}-{date[4:6]}-{date[6:]}:")
-            print("-"*30) # TODO: добавить переменную для этого числа (30)
-
-            for i in range(len(coincidences)):
-                print(f"  {i+1}. {date[0:4]}-{date[4:6]}-{date[6:]} {coincidences[i][9:11]}:{coincidences[i][11:13]} [{coincidences[i][14:-3]}]")
-
-            print("-"*30)
-            # TEST: ПРОТЕСТИРОВАТЬ ЭТУ ДИЧЬ
-            answer = int(input(f"Enter number to delete (1-{len(coincidences)}, or 0 to cancel): "))
-            # WARN: добавить проверку на число
-            if answer != 0:
-                new_answer = input("Are you sure you want to delete this idea? [yn] ")
-                if new_answer.lower() in ["y", "yes"]: # TODO: вынести список из y и yes в отдельную переменную для конфига
-                    print("Deleting idea...")
-                    (folder_ideas / coincidences[answer-1]).unlink()
-
+                (folder_ideas / coincidences[answer-1]).unlink()
 
 def remove_idea_by_name(name):
     pass
@@ -154,12 +131,12 @@ def main():
                 new_idea(command[2], command[3])
         elif command[0] == "remove":
             if command[1] == "idea":
-                if "--date" in command[2:]:
-                    if "--time" in command[2:]:
-                        remove_idea_by_date(command[command.index("--date")+1], command[command.index("--time")+1])
-                    else: remove_idea_by_date(command[command.index("--date")+1])
+                if "--date" in command[2:] and "--time" in command[2:]:
+                    remove_idea_by_date(command[command.index("--date")+1], command[command.index("--time")+1])
+                elif "--date" in command[2:]:
+                    remove_idea_by_date(date=command[command.index("--date")+1])
                 elif "--time" in command[2:]:
-                    print("E: Date Not Entered")
+                    remove_idea_by_date(time=command[command.index("--time")+1])
 
 # TODO: добавить add, remove, rename, rewrite idea
 # добавить название языка в waybar, nvim
