3e8842dde315307b81e4dd456990f57f78a26411
imefo <imefo@yandex.ru>
2026-08-09 00:15:16 +0500

    fix: import module datetime

--- ideas.py
+++ ideas.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from config import *
+from datetime import datetime
 
 # TODO: добавить город в метаданные идей и имя пользователя
 # TODO: добавить триграммы в search & remove
